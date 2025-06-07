38e7bd1ceecd3700348e2bcfb78c847b0d3e3599
Harry Q <87745547+Harry2166@users.noreply.github.com>
2025-06-07 17:02:08 +0800

    feat: access the django API in profileVue

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,5 +1,14 @@
-<script>
-
+<script setup>
+  import axios from 'axios';
+  import { onMounted } from 'vue';
+  onMounted(async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/pokemon/get_all/');
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  })
 </script>
 
 <template>
