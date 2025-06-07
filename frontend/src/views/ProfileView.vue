4a283841eca0e6b1cb2e5b0a4e06c67ae6158b8b
Harry Q <87745547+Harry2166@users.noreply.github.com>
2025-06-07 17:17:19 +0800

    feat: show pokemon in profile

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,10 +1,13 @@
 <script setup>
   import axios from 'axios';
-  import { onMounted } from 'vue';
+  import { ref, onMounted } from 'vue';
+
+  const allPokemon = ref([]);
+
   onMounted(async () => {
     try {
       const response = await axios.get('http://127.0.0.1:8000/pokemon/get_all/');
-      console.log(response.data);
+      allPokemon.value = response.data;
     } catch (error) {
       console.error(error);
     }
@@ -13,4 +16,10 @@
 
 <template>
   <h1>Profile!</h1>
+  <ul>
+    <li v-for="pokemon in allPokemon" :key="pokemon.id">
+      {{pokemon.nickname}}
+      <img :src=pokemon.image_url>
+    </li>
+  </ul>
 </template>
