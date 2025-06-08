f8148f9ce7d976471d33dd8910b90f5d86973414
Harry2166 <harry2166.hq@gmail.com>
2025-06-08 14:16:11 +0800

    feat: Use pokemon cards + use Javascript popup boxes for getting nicknames

--- frontend/src/views/PokemonView.vue
+++ frontend/src/views/PokemonView.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref, onMounted, watch } from 'vue';
   import { availablePokemonStore, ownedPokemonStore, pokemonGenerationStore } from '@/stores/pokemon'
+  import PokemonCard from '@/components/PokemonCard.vue'
   import axios from 'axios'
 
   const ownedPokemon = ownedPokemonStore();
@@ -16,28 +17,37 @@
     6: [649,72],
   }
 
-  const pokemonNicknames = ref({})
   const fetchGenerationPokemon = async () => {
     availablePokemon.fetchPokemon(`https://pokeapi.co/api/v2/pokemon?offset=${generationRanges[pokemonGeneration.generation][0]}&limit=${generationRanges[pokemonGeneration.generation][1]}`);
   }
 
   const getPokemonAPIID = (URL) => { return URL.split("/").filter(Boolean).pop(); }
 
-  const postPokemonToDB = async (URL, pokeAPI_ID) => {
+  const handleCardClick = (pokemon) => {
+    const nickname = window.prompt(`Place a nickname for ${pokemon.name}`, "")
+    if (nickname == "" || nickname  == null) {
+      console.log("no name given")
+    } else {
+      postPokemonToDB(pokemon.url, getPokemonAPIID(pokemon.url), nickname);
+    }
+  };
+
+  const postPokemonToDB = async (URL, pokeAPI_ID, nickname) => {
     const postData = {
       pokeapi_id: pokeAPI_ID,
       image_url:
       `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeAPI_ID}.png`,
-      nickname: pokemonNicknames.value[URL],
+      nickname: nickname,
       gender: true
     }
     try {
       const response = await axios.post('http://127.0.0.1:8000/pokemon/insert/', postData)
       ownedPokemon.fetchPokemon();
       pokemonNicknames.value[URL] = ""
-      console.log("Pokemon added!")
+      window.alert("Pokemon added!")
     } catch {
       console.error(error)
+      window.alert("Pokemon not added!")
     }
   }
 
@@ -55,19 +65,11 @@
     <div
       v-for="pokemon in availablePokemon.pokemonList"
       :id="pokemon.name"
-      class="border rounded p-2 flex flex-col items-center hover:shadow"
       >
-      <label for="pokemonURL" class="block text-sm font-semibold text-gray-700">{{pokemon.name}}</label>
-      <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonAPIID(pokemon.url)}.png`"/>
-      <form @submit.prevent="postPokemonToDB(pokemon.url, getPokemonAPIID(pokemon.url))">
-        <input
-          type="text"
-          id="newPokemon"
-          name="newPokemon"
-          v-model="pokemonNicknames[pokemon.url]"
-          class="mt-1 block w-24 rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-2 py-1"
-        />
-      </form>
+      <PokemonCard :name=pokemon.name
+      :image_url="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonAPIID(pokemon.url)}.png`"
+      @click="handleCardClick(pokemon)"
+      />
     </div>
   </div>
 </template>
