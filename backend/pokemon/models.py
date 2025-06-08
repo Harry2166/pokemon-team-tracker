377c94baadd22240fe0a9bd937996176bce9b7c3
Harry2166 <harry2166.hq@gmail.com>
2025-06-08 21:09:15 +0800

    feat: addd pokemon species name and shiny-ness

--- backend/pokemon/models.py
+++ backend/pokemon/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # SELECT setval(pg_get_serial_sequence('"pokemon_pokemon"', 'id'), MAX(id)) FROM pokemon_pokemon;
 class Pokemon(models.Model):
+    species_name = models.CharField(default="", max_length=255)
     pokeapi_id = models.IntegerField(default=0)
     nickname = models.CharField(max_length=255)
     gender = models.BooleanField() # true = male, false = female
     image_url = models.URLField()
     captured_at = models.DateTimeField(auto_now_add=True)
+    shiny = models.BooleanField(default=False)
 
