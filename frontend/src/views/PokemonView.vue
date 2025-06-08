bd22fbd21f246702166c7f21cdd45a4cfe455977
Harry Q <87745547+Harry2166@users.noreply.github.com>
2025-06-08 11:18:51 +0800

    feat: input custom nicknames to pokemon

--- frontend/src/views/PokemonView.vue
+++ frontend/src/views/PokemonView.vue
@@ -16,23 +16,26 @@
     6: [649,72],
   }
 
-
+  const pokemonNicknames = ref({})
   const fetchGenerationPokemon = async () => {
     availablePokemon.fetchPokemon(`https://pokeapi.co/api/v2/pokemon?offset=${generationRanges[pokemonGeneration.generation][0]}&limit=${generationRanges[pokemonGeneration.generation][1]}`);
   }
 
-  const postPokemonToDB = async (URL) => {
-    const pokeapi_ID = URL.split("/").filter(Boolean).pop();
+  const getPokemonAPIID = (URL) => { return URL.split("/").filter(Boolean).pop(); }
+
+  const postPokemonToDB = async (URL, pokeAPI_ID) => {
     const postData = {
-      pokeapi_id: pokeapi_ID,
+      pokeapi_id: pokeAPI_ID,
       image_url:
-      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeapi_ID}.png`,
-      nickname: "Rick Roll",
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeAPI_ID}.png`,
+      nickname: pokemonNicknames.value[URL],
       gender: true
     }
     try {
       const response = await axios.post('http://127.0.0.1:8000/pokemon/insert/', postData)
       ownedPokemon.fetchPokemon();
+      pokemonNicknames.value[URL] = ""
+      console.log("Pokemon added!")
     } catch {
       console.error(error)
     }
@@ -47,9 +50,20 @@
   <h1>Generation {{pokemonGeneration.generation}}</h1>
   <button @click="pokemonGeneration.decreaseGeneration">-</button>
   <ul>
-    <li v-for="pokemon in availablePokemon.pokemonList">
-      {{pokemon.name}}
-      <button @click="postPokemonToDB(pokemon.url)">Catch {{pokemon.name}}</button>
+    <li v-for="pokemon in availablePokemon.pokemonList" :id="pokemon.name">
+      <label for="pokemonURL" class="block text-sm font-semibold text-gray-700">{{pokemon.name}}</label>
+      <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonAPIID(pokemon.url)}.png`"/>
+      <form @submit.prevent="postPokemonToDB(pokemon.url, getPokemonAPIID(pokemon.url))">
+        <input
+          type="text"
+          id="newPokemon"
+          name="newPokemon"
+          v-model="pokemonNicknames[pokemon.url]"
+          class="mt-1 block w-half rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-3 py-2"
+        />
+        <br>
+        <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded shadow">Catch {{pokemon.name}}</button>
+      </form>
     </li>
   </ul>
 </template>
